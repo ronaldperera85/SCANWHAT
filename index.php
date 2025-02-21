1a9e7d30872ab8f8001be7dbd1d98d540c448947
unknown <ronaldperera85@gmail.com>
2025-02-21 17:02:34 -0400

    redireccion_alertas_detallesv1

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: pages/login.php");
     exit;
 }
+
+// Definir la ruta al favicon (ajusta la ruta si es necesario)
+$faviconPath = "img/small.png";
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -13,6 +16,8 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SCANWHAT</title>
+    <!-- Favicon -->
+    <link rel="icon" href="<?php echo $faviconPath; ?>" type="image/x-icon">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
           integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
